db286ccf1aedfdb0c6ab7a21b28cfe01b27bd182
uesleipedro <ielseu@hotmail.com>
2022-05-16 01:10:51 -0300

    teste de geracao de xml

--- src/routes/index.php
+++ src/routes/index.php
@@ -1,11 +1,12 @@
 <?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 use Slim\Factory\AppFactory;
-use NFePHP\DA\NFe\DanfeSimples;
-
-//require __DIR__ . '/../vendor/autoload.php';
+use App\Model\Dfe;
 
 $app = AppFactory::create();
 
@@ -16,12 +17,10 @@ $app->get('/', function (Request $request, Response $response, $args) {
 
 $app->group('/nfe', function (RouteCollectorProxy $group) {
 
-  $group->get('', function(Request $request, Response $response) {
-
-    $response->getBody()->write("Hello group");
+  $group->get('', function (Request $request, Response $response) {
+    $response->getBody()->write(json_encode(Dfe::emitir()));
     return $response;
   });
-
 });
 
-$app->run(); 
+$app->run();
