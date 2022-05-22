8c0f658f6e714b814e11cc27176720456326eadd
uesleipedro <ielseu@hotmail.com>
2022-05-22 15:25:26 -0300

    erroDfe

--- src/Model/Dfe.php
+++ src/Model/Dfe.php
@@ -30,7 +30,7 @@ class Dfe
     $std->natOp = 'VENDA';
     //$std->indPag = 0; //NÃO EXISTE MAIS NA VERSÃO 4.00
     $std->mod = 55;
-    $std->serie = 1;
+    $std->serie = 3;
     $std->nNF = 2;
     $std->dhEmi = '2015-02-19T13:48:00-02:00';
     $std->dhSaiEnt = null;
@@ -113,18 +113,18 @@ class Dfe
     $std->item = 1;
     $std->cProd = '0001';
     //$std->cEAN;
-    //$std->cBarra;
+    //$std->cBarra; ///não existe no sistema da sempre
     $std->xProd = "Produto teste";
     $std->NCM = '66554433';
-    //$std->cBenef;
-    //$std->EXTIPI;
+    //$std->cBenef; //inclusa no layout 4.00
+    //$std->EXTIPI; //incluso no layout 4.00
     $std->CFOP = '5102';
     $std->uCom = 'PÇ';
     $std->qCom = '1.0000';
     $std->vUnCom = '10.99';
     $std->vProd = '10.99';
     //$std->cEANTrib;
-    //$std->cBarraTrib;
+    //$std->cBarraTrib; //não exite no sistema da sempre
     $std->uTrib = 'PÇ';
     $std->qTrib = '1.0000';
     $std->vUnTrib = '10.99';
@@ -134,7 +134,8 @@ class Dfe
     //$std->vOutro;
     $std->indTot = 1;
     //$std->xPed;
-    //$std->nItemPed;
+    //$std->nItemPed; /// não existe no sitema da sempre
+    //$std->nFCI; só tinha no da sempre
     $nfe->tagprod($std);
 
     /**
@@ -153,44 +154,92 @@ class Dfe
     $std->vBC = 0.20;
     $std->pICMS = '18.0000';
     $std->vICMS = '0.04';
+    //$std->vFCP;
+    //$std->vBCFCP;
+    //$std->modBCST;
+    //$std->pMVAST;
+    //$std->pRedBCST;
+    //$std->vBCST;
+    //$std->pICMSST;
+    //$std->vICMSST;
+    //$std->vBCFCPST;
+    //$std->pFCPST;
+    //$std->vFCPST;
+    //$std->vICMSDeson;
+    //$std->motDesICMS;
+    //$std->pRedBC;
+    //$std->vICMSOp;
+    //$std->pDif;
+    //$std->vICMSDif;
+    //$std->vBCSTRet;
+    //$std->pST;
+    //$std->vICMSSTRet;
+    //$std->vBCFCPSTRet;
+    //$std->pFCPSTRet;
+    //$std->vFCPSTRet;
+    //$std->pRedBCEfet;
+    //$std->vBCEfet;
+    //$std->pICMSEfet;
+    //$std->vICMSEfet;
+    //$std->vICMSSubstituto; //NT 2020.005 v1.20
+    //$std->vICMSSTDeson; //NT 2020.005 v1.20
+    //$std->motDesICMSST; //NT 2020.005 v1.20
+    //$std->pFCPDif; //NT 2020.005 v1.20
+    //$std->vFCPDif; //NT 2020.005 v1.20
+    //$std->vFCPEfet; //NT 2020.005 v1.20
     $nfe->tagICMS($std);
 
     $std = new \stdClass();
-    $std->item = 1;
+    $std->item = 1; //item da NFe
+    $std->clEnq = null;
+    $std->CNPJProd = null;
+    $std->cSelo = null;
+    $std->qSelo = null;
     $std->cEnq = '999';
     $std->CST = '50';
     $std->vIPI = 0;
     $std->vBC = 0;
     $std->pIPI = 0;
+    $std->qUnid = null;
+    $std->vUnid = null;
     $nfe->tagIPI($std);
 
     $std = new \stdClass();
-    $std->item = 1;
+    $std->item = 1; //item da NFe
     $std->CST = '07';
-    $std->vBC = 0;
-    $std->pPIS = 0;
-    $std->vPIS = 0;
+    $std->vBC = null;
+    $std->pPIS = null;
+    $std->vPIS = null;
+    $std->qBCProd = null;
+    $std->vAliqProd = null;
     $nfe->tagPIS($std);
 
     $std = new \stdClass();
-    $std->item = 1;
-    $std->vCOFINS = 0;
-    $std->vBC = 0;
-    $std->pCOFINS = 0;
+    $std->item = 1; //item da NFe
+    $std->CST = '07';
+    $std->vBC = null;
+    $std->pCOFINS = null;
+    $std->vCOFINS = null;
+    $std->qBCProd = null;
+    $std->vAliqProd = null;
     $nfe->tagCOFINSST($std);
 
     $std = new \stdClass();
     $std->vBC = 0.20;
     $std->vICMS = 0.04;
     $std->vICMSDeson = 0.00;
+    $std->vFCP = 0.00; //incluso no layout 4.00
     $std->vBCST = 0.00;
     $std->vST = 0.00;
+    $std->vFCPST = 0.00; //incluso no layout 4.00
+    $std->vFCPSTRet = 0.00; //incluso no layout 4.00
     $std->vProd = 10.99;
     $std->vFrete = 0.00;
     $std->vSeg = 0.00;
     $std->vDesc = 0.00;
     $std->vII = 0.00;
     $std->vIPI = 0.00;
+    $std->vIPIDevol = 0.00; //incluso no layout 4.00
     $std->vPIS = 0.00;
     $std->vCOFINS = 0.00;
     $std->vOutro = 0.00;
@@ -215,6 +264,7 @@ class Dfe
     $std = new \stdClass();
     $std->nFat = '100';
     $std->vOrig = 100;
+    $std->vDesc = null;
     $std->vLiq = 100;
     $nfe->tagfat($std);
 
@@ -226,6 +276,7 @@ class Dfe
 
     $xml = $nfe->getXML(); // O conteúdo do XML fica armazenado na variável $xml
 
+    //$xml = $nfe->getErrors(); // O conteúdo do XML fica armazenado na variável $xml
     return $xml;
   }
 
@@ -233,7 +284,7 @@ class Dfe
 
     $xml = (new self)->createXML();
     $config = [
-      "atualizacao" => "2018-02-06 06:01:21",
+      "atualizacao" => "2022-05-22 15:17:21",
       "tpAmb" => 2, // Se deixar o tpAmb como 2 você emitirá a nota em ambiente de homologação(teste) e as notas fiscais aqui não tem valor fiscal
       "razaosocial" => "Empresa teste",
       "siglaUF" => "RS",
